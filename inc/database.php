223aa5263c2561b4aac9a3f01c42e0dc876bfc49
Stephan Bösch-Plepelits <skunk@cg.tuwien.ac.at>
2015-06-10 13:48:32 +0200

    DB: always use fetch assoc

--- inc/database.php
+++ inc/database.php
@@ -1,5 +1,6 @@
 <?php
 $db_conn = new PDO("sqlite:{$db['path']}");
+$db_conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
 function db_quote_ident($str) {
   return "\"{$str}\"";
