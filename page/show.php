e6170decbf773d5f939b3a1f71233401a8020afc
Stephan Bösch-Plepelits <skunk@cg.tuwien.ac.at>
2015-06-11 11:43:23 +0200

    Page Show: transpose table

--- page/show.php
+++ page/show.php
@@ -8,7 +8,7 @@ class Page_show extends Page {
 
     $def = $type->def();
     $table = new table($def, array($object->view()), array("template_engine"=>"twig"));
-    $ret .= $table->show();
+    $ret .= $table->show("html-transposed");
 
     $ret .= "<a href='" . page_url(array("page" => "edit", "type" => $param['type'], "id" => $param['id'] )) . "'>Edit</a>\n|";
     $ret .= "<a href='" . page_url(array("page" => "list", "type" => $param['type'] )) . "'>Back</a>\n";
