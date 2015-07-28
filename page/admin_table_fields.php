4d6f4689dbc586674695811ef6b9705aa9610e27
Stephan Bösch-Plepelits <skunk@cg.tuwien.ac.at>
2015-07-28 12:00:11 +0200

    Page Admin Table Fields: don't rely on form_template_editor(), use FieldTypes

--- page/admin_table_fields.php
+++ page/admin_table_fields.php
@@ -10,9 +10,17 @@ class Page_admin_table_fields extends Page {
     foreach(get_db_tables() as $t)
       $tables_data[$t->id] = $t->view();
 
-    $template_options = array(
-      'def_form' => array(
-	'key_def' => array(
+    $has_values = array("check", "type", array("or", array("is", "radio"), array("is", "select"), array("is", "checkbox"), array("is", "text"), array("is", "autocomplete"), array("is", "keywords")));
+    $field_types = get_field_types();
+
+    $def = array(
+      'fields'	=> array(
+	'name'	=>"Fields",
+	'type'	=>"hash",
+	'default'   =>1,
+	'hide_label'=>true,
+
+	'key_def'	=>array(
 	  'name'  =>lang('form:hash_key_field_name'),
 	  'default_func' => array(
 	    'js' => <<<EOT
@@ -35,33 +43,60 @@ EOT
 	  'req'	=>true,
 	  'check'	=>array("regexp", "^[a-zA-Z0-9_]+$", "Use only characters, digits and underscores."),
 	),
-      ),
-      'def_additional' => array(
-	'count' => array(
-	  'type' => 'select',
-	  'name' => 'Multiple values',
-	  'values' => array(
-	    'no' => 'single value',
-	    'ordered' => 'multiple values, ordered',
-	    'unordered' => 'multiple values, unordered',
+	'def'       =>array(
+	  'type'      =>'form',
+	  'def'       => array(
+	    'name'	=>array(
+	      'name'	=>"Name",
+	      'type'	=>"text",
+	    ),
+	    'type'	=>array(
+	      'name'	=>"Type",
+	      'type'	=>"select",
+	      'values_mode' => 'keys',
+	      'values'	=>array_map(function($f) {
+		return $f->type();
+	      }, $field_types),
+	      'default'     =>'text',
+	    ),
+	    'values'	=>array(
+	      'name'	=>"Values",
+	      'type'	=>"hash",
+	      'def'	=>array(
+		'name'	=>lang('form:hash_value_field_name'),
+		'type'	=>"text",
+	      ),
+	      'default'=>0,
+	      'button:add_element' => "Add value",
+	      'show_depend'=>$has_values,
+	      // 'include_data'=>array('and', array('not_empty'), $has_values),
+	      'include_data'=>$has_values,
+	    ),
+	    'count' => array(
+	      'type' => 'select',
+	      'name' => 'Multiple values',
+	      'values' => array(
+		'no' => 'single value',
+		'ordered' => 'multiple values, ordered',
+		'unordered' => 'multiple values, unordered',
+	      ),
+	      'default' => 'no',
+	      'req' => true,
+	    ),
+	    'old_key' => array(
+	      'type' => 'hidden',
+	    ),
+	    'reference' => array(
+	      'type'	=> 'select',
+	      'req'		=> false,
+	      'name'	=> 'Reference',
+	      'desc'	=> 'Use this to reference another table as possibles values for this field. Leave \'Values\' empty.',
+	      'values'	=> $tables_data,
+	    ),
 	  ),
-	  'default' => 'no',
-	  'req' => true,
-	),
-        'old_key' => array(
-	  'type' => 'hidden',
-	),
-	'reference' => array(
-	  'type'	=> 'select',
-	  'req'		=> false,
-	  'name'	=> 'Reference',
-	  'desc'	=> 'Use this to reference another table as possibles values for this field. Leave \'Values\' empty.',
-	  'values'	=> $tables_data,
 	),
       ),
     );
-
-    $def = form_template_editor($template_options);
 
     $form = new form("data", $def);
 
