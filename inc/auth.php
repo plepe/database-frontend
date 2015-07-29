91a14663f573956cea82c59dce27b43158484127
Stephan Bösch-Plepelits <skunk@cg.tuwien.ac.at>
2015-07-29 10:54:38 +0200

    Auth: show user info / login/logout links

--- inc/auth.php
+++ inc/auth.php
@@ -4,3 +4,14 @@ register_hook("init", function() {
 
   $auth = new Auth();
 });
+
+function auth_user_info() {
+  global $auth;
+
+  if($auth->is_logged_in()) {
+    return "Logged in as " . $auth->current_user()->name() . ". <a href='" . page_url(array("page" => "logout")) . "'>Logout</a>";
+  }
+  else {
+    return "<a href='" . page_url(array("page" => "login")) . "'>Login</a>";
+  }
+}
