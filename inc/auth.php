b4885a607e12e3af3750f060d742d91c4c841cc2
Stephan Bösch-Plepelits <skunk@cg.tuwien.ac.at>
2015-07-31 13:56:35 +0200

    Auth: authentication via REMOTE_USER (HTTP Auth Basic)

--- inc/auth.php
+++ inc/auth.php
@@ -3,15 +3,26 @@ register_hook("init", function() {
   global $auth;
 
   $auth = new Auth();
+
+  if(isset($_SERVER['REMOTE_USER'])) {
+    $user = $auth->get_user($_SERVER['REMOTE_USER']);
+    $auth->set_current_user($user);
+  }
 });
 
 function auth_user_info() {
   global $auth;
+  $ret = "";
 
   if($auth->is_logged_in()) {
-    return "Logged in as " . $auth->current_user()->name() . ". <a href='" . page_url(array("page" => "logout")) . "'>Logout</a>";
+    $ret .= "Logged in as " . $auth->current_user()->name() . ".";
+    if(!isset($_SERVER['REMOTE_USER']))
+      $ret .= " <a href='" . page_url(array("page" => "logout")) . "'>Logout</a>";
   }
   else {
-    return "<a href='" . page_url(array("page" => "login")) . "'>Login</a>";
+    if(!isset($_SERVER['REMOTE_USER']))
+      $ret .= "<a href='" . page_url(array("page" => "login")) . "'>Login</a>";
   }
+
+  return $ret;
 }
