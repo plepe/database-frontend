a1accba151292e33f83f1612869dbe41cc06afb2
Stephan Bösch-Plepelits <skunk@cg.tuwien.ac.at>
2015-08-06 13:49:21 +0200

    Page import&export: show full data as json & save

---
+++ page/import_export.php
@@ -0,0 +1,53 @@
+<?php
+class Page_import_export extends Page {
+  function content($param) {
+    $form = new form("data", array(
+      'json' => array(
+        'type' => 'json',
+	'name' => 'JSON',
+      ),
+    ));
+
+    if($form->is_complete()) {
+      $data = $form->get_data();
+      $data = $data['json'];
+
+      foreach($data['__system__'] as $d) {
+	$table = new DB_Table();
+	$table->save($d);
+      }
+
+      foreach($data as $k => $entries) {
+	if($k == "__system__")
+	  continue;
+	
+	foreach($entries as $d) {
+	  $ob = new DB_Entry($k, null);
+	  $ob->save($d);
+	}
+      }
+    }
+
+    if($form->is_empty()) {
+      $data = array('__system__' => array());
+
+      foreach(get_db_tables() as $table) {
+	$data['__system__'][$table->id] = $table->data();
+
+	$data[$table->id] = array();
+
+	foreach(get_db_entries($table->id) as $entry) {
+	  $data[$table->id][] = $entry->data();
+	}
+      }
+
+      $form->set_data(array("json" => $data));
+    }
+
+    return 
+      "<form method='post'>\n" .
+      $form->show() .
+      "<input type='submit' value='Save'/>\n" .
+      "</form>\n";
+  }
+}
