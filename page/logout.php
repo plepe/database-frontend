9e646556335bc5a2f0c566592a8325850881598f
Stephan Bösch-Plepelits <skunk@cg.tuwien.ac.at>
2015-08-17 11:32:20 +0200

    Page logout: also redirect to return_to or http referer

--- page/logout.php
+++ page/logout.php
@@ -4,7 +4,16 @@ class Page_logout {
     global $auth;
 
     $auth->clear_authentication();
-    page_reload(array());
+
+    if(array_key_exists('return_to', $param)) {
+      page_reload($param['return_to']);
+    }
+    elseif(array_key_exists('HTTP_REFERER', $_SERVER)) {
+      page_reload($_SERVER['HTTP_REFERER']);
+    }
+    else {
+      page_reload(array());
+    }
 
     return "You have been logged out.";
   }
