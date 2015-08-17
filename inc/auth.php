92b0e0348c5d3ad6a6f7e2ec00371553eceee0a1
Stephan Bösch-Plepelits <skunk@cg.tuwien.ac.at>
2015-08-17 10:48:50 +0200

    Auth: define $base_access in conf.php

--- inc/auth.php
+++ inc/auth.php
@@ -26,3 +26,16 @@ function auth_user_info() {
 
   return $ret;
 }
+
+function base_access($access) {
+  global $auth;
+  global $base_access;
+
+  if(!isset($base_access))
+    return true;
+
+  if(!array_key_exists($access, $base_access))
+    return true;
+
+  return $auth->access($base_access[$access]);
+}
