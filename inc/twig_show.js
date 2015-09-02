233bf3509e7bdcb24de86a440669325c30e27e2e
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2015-09-02 21:55:44 +0200

    (JS) Twig: template get_entry, get_entries functions

--- inc/twig_show.js
+++ inc/twig_show.js
@@ -4,4 +4,10 @@ register_hook('twig_init', function() {
 
   Twig.extendFilter("show_list", function(data) {
   });
+
+  Twig.extendFunction("get_entry", function(data) {
+  });
+
+  Twig.extendFunction("get_entries", function(data) {
+  });
 });
