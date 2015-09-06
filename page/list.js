f74babfd444962ce4524a08c03582a876e6a2d0e
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2015-09-06 21:47:32 +0200

    (JS) Page List: page.param

--- page/list.js
+++ page/list.js
@@ -2,29 +2,41 @@ function Page_list() {
 }
 
 Page_list.prototype.content = function(param) {
+  this.param = param;
+
+  if(!this.param.limit)
+    this.param.limit = 25;
+  if(!this.param.offset)
+    this.param.offset = 0;
+
   return {
-    'template': 'list.html'
+    'template': 'list.html',
+    'table': this.param.table
   };
 }
 
 Page_list.prototype.connect = function(param) {
-  get_db_table(param.table, function(param, ob) {
+  this.param = param;
+
+  get_db_table(this.param.table, function(ob) {
     this.db_table = ob;
     this.data = new DB_TableExtract(this.db_table);
 
-    var view = this.db_table.default_view('list');
-    var def = this.db_table.view_def(view);
+    if(!this.param.view)
+      this.param.view = this.db_table.default_view('list');
+
+    var def = this.db_table.view_def(this.param.view);
     var t = new table(def.fields, this.data, { 'template_engine': 'twig' });
 
     t.show('html',
     {
-      'limit': param.limit || 25,
-      'offset': param.offset || 0
+      'limit': this.param.limit,
+      'offset': this.param.offset
     },
     function(r) {
       var x = document.getElementsByClassName('table_wrapper');
       x[0].innerHTML = r;
     });
 
-  }.bind(this, param));
+  }.bind(this));
 }
