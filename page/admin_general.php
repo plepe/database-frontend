b8a44340b113ba2b83417b77a3b217e207827ad4
Stephan Bösch-Plepelits <skunk@cg.tuwien.ac.at>
2015-09-21 11:13:29 +0200

    Page Admin General: new page, set general settings

---
+++ page/admin_general.php
@@ -0,0 +1,45 @@
+<?php
+class Page_admin_general extends Page {
+  function content($param) {
+    global $system;
+
+    if(!base_access('admin')) {
+      global $auth;
+      if(!$auth->is_logged_in())
+	page_reload(array("page" => "login", "return_to" => array("page" => "admin_general")));
+      return "Permission denied.";
+    }
+
+    foreach(get_db_tables() as $t)
+      $table_names[$t->id] = $t->name();
+
+
+    $def = array(
+    );
+
+    $form = new form("data", $def);
+
+    if($form->is_complete()) {
+      $data = $form->get_data();
+      
+      $result = $system->save($data, $param['message']);
+
+      if($result === true) {
+	page_reload(page_url(array("page" => "admin_general")));
+      }
+      else {
+	messages_add("An error occured while saving: {$result}", MSG_ERROR);
+      }
+    }
+    
+    if($form->is_empty()) {
+      $form->set_data($system->data());
+    }
+
+    return array(
+      'template' => 'admin_general.html',
+      'form' => $form,
+      'data' => $system->view(),
+    );
+  }
+}
