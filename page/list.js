4907c9fdf472d920404e146f926536b6888b60d0
Stephan Bösch-Plepelits <skunk@cg.tuwien.ac.at>
2016-05-13 13:48:21 +0200

    test

--- page/list.js
+++ page/list.js
@@ -1,3 +1,36 @@
+register_hook("twig_init", function() {
+  Twig.extendFunction('panel_items', function(data) {
+    return 'FOO';
+  });
+
+  Twig.extendFilter('show', function(data) {
+    return 'FOO';
+  });
+
+  Twig.extendFilter('show_list', function(view) {
+    console.log(view);
+    return view.show_list();
+  });
+});
+
+function View_Table(def, param) {
+  this.def = def;
+  this.param = param;
+}
+
+View_Table.prototype.set_extract = function(extract) {
+  this.extract = extract;
+}
+
+View_Table.prototype.show = function() {
+}
+
+View_Table.prototype.show_list = function() {
+  this.view = new table(this.def.fields, this.extract, { "template_engine": "twig" });
+
+  return this.view.show('html', this.param);
+}
+
 function Page_list() {
 }
 
@@ -6,7 +39,7 @@ Page_list.prototype.content = function(param) {
     template: "list.html"
   };
   
-  ret.content = "FOOBAR";
+  ret.view = new View_Table({}, param);
 
   return ret;
 }
