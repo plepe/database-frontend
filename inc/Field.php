b92064a259ffbd1df7eab48dc5222526df5a7b38
Stephan Bösch-Plepelits <skunk@cg.tuwien.ac.at>
2017-06-20 09:22:57 +0200

    Field->name() function

--- inc/Field.php
+++ inc/Field.php
@@ -10,6 +10,10 @@ class Field {
     return substr(get_called_class(), 6);
   }
 
+  function name() {
+    return $this->def['name'];
+  }
+
   function form_type() {
     return substr(get_called_class(), 6);
   }
