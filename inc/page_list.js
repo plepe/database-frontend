4970a11d9068dee68aa4b12de3be1d105c318ce2
Stephan Bösch-Plepelits <skunk@cg.tuwien.ac.at>
2017-06-22 13:22:34 +0200

    Page List: Connect pager

--- inc/page_list.js
+++ inc/page_list.js
@@ -1,4 +1,33 @@
-function page_list_reload() {
+function page_list_connect () {
+  var pager = document.getElementsByClassName('Pager')
+  var result_count = parseInt(document.getElementsByClassName('result_count')[0].textContent)
+  for (var i = 0; i < pager.length; i++) {
+    var links = pager[i].getElementsByClassName('LinkButton')
+    links[0].onclick = function () {
+      param.offset = 0
+      page_list_reload()
+      return false
+    }
+    links[1].onclick = function () {
+      param.offset = Math.max(0, parseInt(param.offset) - parseInt(param.limit))
+      page_list_reload()
+      return false
+    }
+    links[2].onclick = function () {
+      param.offset = Math.min(result_count - result_count % parseInt(param.limit), parseInt(param.offset) + parseInt(param.limit))
+      page_list_reload()
+      return false
+    }
+    links[3].onclick = function () {
+      param.offset = result_count - result_count % parseInt(param.limit)
+      page_list_reload()
+      return false
+    }
+  }
+
+}
+
+function page_list_reload () {
   var req = new XMLHttpRequest()
   
   req.addEventListener('load', function () {
@@ -7,6 +36,14 @@ function page_list_reload() {
     var table_new = text.getElementById('table')
     var table_old = document.getElementById('table')
     table_old.innerHTML = table_new.innerHTML
+
+    var pager_new = text.getElementsByClassName('Pager')
+    var pager_old = document.getElementsByClassName('Pager')
+    for (var i = 0; i < pager_new.length; i++) {
+      pager_old[i].innerHTML = pager_new[i].innerHTML
+    }
+
+    page_list_connect()
   })
 
   req.addEventListener('error', function () {
@@ -42,5 +79,6 @@ register_hook('init', function () {
       page_list_reload()
     }
     form__.onchange = page_list_reload
+    page_list_connect()
   }
 })
