78b0e6dbebcbefef1ff8d20572531231a57f0f48
Stephan Bösch-Plepelits <skunk@cg.tuwien.ac.at>
2017-09-28 12:01:11 +0200

    List View: link first cell
    - always link contents of first cell to show page
    - add a pencil icon for editing

--- inc/View_Table.php
+++ inc/View_Table.php
@@ -6,17 +6,16 @@ class View_Table extends View {
   }
 
   function show_list() {
-    $this->def['fields']['__links'] = array(
-      "name" => "",
-      "format" => 
-        "<a class='TableLink' href='" .  
-	strtr(page_url(array('page' => 'show', 'table' => $this->param['table'], 'id' => "ID")
-	), array("ID" => "{{ id }}")) .
-	"'>Show</a> <a class='TableLink' href='" .
-	strtr(page_url(array('page' => 'edit', 'table' => $this->param['table'], 'id' => "ID")
-	), array("ID" => "{{ id }}")) .
-	"'>Edit</a>",
-    );
+    $first_field = array_keys($this->def['fields'])[0];
+    $this->def['fields'][$first_field]['format'] =
+      "<a class='TableLink' href='" .
+      strtr(page_url(array('page' => 'show', 'table' => $this->param['table'], 'id' => "ID")), array("ID" => "{{ id }}")) .
+      "'>" .
+      $this->def['fields'][$first_field]['format'] .
+      "</a>" .
+      "<a title='edit' class='edit' href='" .
+      strtr(page_url(array('page' => 'edit', 'table' => $this->param['table'], 'id' => "ID")), array("ID" => "{{ id }}")) .
+      "'><img src='images/edit.png'></a>";
 
     $view = new table($this->def['fields'], $this->extract, array("template_engine"=>"twig"));
 
