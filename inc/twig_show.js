9e48a61ad14b3f301a8fd51317cc6c301ac69914
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2020-03-31 13:40:58 +0200

    twig_show(JS): start implementation

--- inc/twig_show.js
+++ inc/twig_show.js
@@ -2,20 +2,36 @@ const Twig = require('twig')
 
 module.exports = {
   init () {
-    Twig.extendFilter("show", param => {
+    Twig.extendFilter("show", (value, param) => {
+      if (!value) {
+        return ''
+      }
+
+      return value.show(param)
     })
 
     Twig.extendFilter("show_list", (value, param) => {
+      if (!value) {
+        return ''
+      }
+
       return value.show_list(param)
     })
 
-    Twig.extendFilter("entry_title", param => {
+    Twig.extendFunction("entry_title", (table, id) => {
+      if (id == null) {
+        return ''
+      }
+
+      return table + '/' + id
     })
 
-    Twig.extendFilter("get_entry", param => {
+    Twig.extendFunction("get_entry", (table, id) => {
+      return 'get_entry("' + table + '", "' + id + '")'
     })
 
-    Twig.extendFilter("get_entries", param => {
+    Twig.extendFunction("get_entries", (table, filter) => {
+      return 'get_entries("' + table + '", ' + JSON.stringify(filter) + ')'
     })
 
     Twig.extendFilter("age", param => {
