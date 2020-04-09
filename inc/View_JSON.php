48d3233fcd4f7d04b5c7fd9cea10526385e818dd
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2020-04-09 21:44:40 +0200

    View_JSON(PHP): bugfix

--- inc/View_JSON.php
+++ inc/View_JSON.php
@@ -1,11 +1,11 @@
 <?php
 class View_JSON extends View {
   function show() {
-    $object = $this->extract->get(0, 1);
+    $object = array_values($this->extract->get(0, 1))[0];
 
     return
       "<pre class='view_json'>\n" .
-      htmlspecialchars(json_readable_encode($object[0]->view())) .
+      htmlspecialchars(json_readable_encode($object->view())) .
       "</pre>\n";
   }
 }
