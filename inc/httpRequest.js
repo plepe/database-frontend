7d321d8e86c6cab05929ab665b1a1f9a5fb0f7a0
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2020-04-11 09:38:14 +0200

    httpRequest: option 'body': request data

--- inc/httpRequest.js
+++ inc/httpRequest.js
@@ -2,6 +2,7 @@
  * @param {string} url - The URL of the request
  * @param {Object} options - Options
  * @param {string} options.method=GET - Method of the request (e.g. POST, DELETE).
+ * @param {string} options.body=null - Body of the request.
  * @param {boolean} options.forceServerLoad=false - Pass the request via the httpRequest.php server script to avoid CORS problems.
  * @param {function} callback - Callback which will be called when the request completes
  * @param {Error} callback.err - If an error occured, the error. Otherwise null.
@@ -32,7 +33,7 @@ function httpRequest (url, options, callback) {
     xhr.open(options.method || 'GET', url, true)
     xhr.responseType = 'text'
     xhr.onreadystatechange = readyStateChange
-    xhr.send()
+    xhr.send(options.body)
   }
 
   function viaServer () {
@@ -40,7 +41,7 @@ function httpRequest (url, options, callback) {
     xhr.open(options.method || 'GET', 'httpGet.php?url=' + encodeURIComponent(url), true)
     xhr.responseType = 'text'
     xhr.onreadystatechange = readyStateChange
-    xhr.send()
+    xhr.send(options.body)
   }
 
   if (options.forceServerLoad) {
