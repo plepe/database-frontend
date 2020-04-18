67f9619e14ba845d5f595df7d920d982c5aadba2
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2020-04-18 09:19:51 +0200

    Timestamp API: wait n seconds until change occured (testing every 5 seconds)

--- ts.php
+++ ts.php
@@ -4,6 +4,18 @@
 <?php call_hooks("init"); ?>
 <?php Header('Content-Type: text/plain; charset=utf-8'); ?>
 <?php
+session_write_close();
+
 $result = get_timestamps(array_key_exists('ts', $_REQUEST) ? $_REQUEST['ts'] : null);
+
+if (array_key_exists('wait', $_REQUEST) && array_key_exists('ts', $_REQUEST) && $_REQUEST['wait'] && $_REQUEST['ts']) {
+  $wait = $_REQUEST['wait'];
+  $step = 5;
+  while ($result['ts'] === $_REQUEST['ts'] && $wait > 0) {
+    sleep($step);
+    $result = get_timestamps($_REQUEST['ts']);
+    $wait -= $step;
+  }
+}
 
 print json_readable_encode($result);
