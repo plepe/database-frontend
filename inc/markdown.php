80774873d78eec823ada66a7ca10c7f6f5f7f75e
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2020-04-26 07:20:50 +0200

    Markdown: try to use single marked process

--- inc/markdown.php
+++ inc/markdown.php
@@ -12,26 +12,35 @@ class Field_markdown extends Field {
   }
 }
 
-register_hook("twig_init", function() {
-  global $twig;
-  $twig->addFilter(new Twig_SimpleFilter('markdown', function($value) {
+function markdown_format ($value) {
+  global $markdown_pipes;
+
+  if (!isset($markdown_pipes)) {
     $descspec = array(
       0 => array('pipe', 'r'),
       1 => array('pipe', 'w')
     );
 
-    $proc = proc_open('bin/marked', $descspec, $pipes);
+    $proc = proc_open('bin/marked', $descspec, $markdown_pipes);
 
-    if (is_resource($proc)) {
-      fwrite($pipes[0], $value);
-      fclose($pipes[0]);
-
-      $result = stream_get_contents($pipes[1]);
-
-      fclose($pipes[1]);
+    if (!is_resource($proc)) {
+      messages_add("Can't start marked process");
     }
+  }
 
-    proc_close($proc);
-    return $result;
+  fwrite($markdown_pipes[0], strtr($value, array("\\" => "\\\\", "\n" => "\\n")) . "\n");
+  fflush($markdown_pipes[0]);
+  $result = fgets($markdown_pipes[1]);
+
+  $result = strtr($value, array("\\\\" => "\\", "\\n" => "\n"));
+
+  return $result;
+}
+
+
+register_hook("twig_init", function() {
+  global $twig;
+  $twig->addFilter(new Twig_SimpleFilter('markdown', function($value) {
+    return markdown_format($value)
   }, array("is_safe"=>array("html"))));
 });
