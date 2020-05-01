14932bd70918e7307a0adc4f281667d1dcf61f20
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2020-05-01 07:06:01 +0200

    DBF Age: move twig filter 'age' to separate module

--- inc/twig_show.js
+++ inc/twig_show.js
@@ -1,5 +1,4 @@
 const Twig = require('twig')
-const htmlentities = require('html-escaper').escape
 
 module.exports = {
   init () {
@@ -58,38 +57,6 @@ module.exports = {
       } else {
         return []
       }
-    })
-
-    Twig.extendFilter("age", date => {
-      if (date == null) {
-        return ''
-      }
-
-      let now = new Date()
-      let diff = (now - new Date(date)) / 1000
-      let text
-
-      if (diff < 0) {
-        text = 'not yet'
-      } else if (diff < 2 * 60) {
-        text = 'just now'
-      } else if (diff < 45 * 60) {
-        text = Math.round(diff / 60) + ' minutes ago'
-      } else if (diff < 90 * 60) {
-        text = 'an hour ago'
-      } else if (diff < 86400) {
-        text = Math.round(diff / 3600) + ' hours ago'
-      } else if (diff < 2 * 86400) {
-        text = 'yesterday'
-      } else if (diff < 61 * 86400) {
-        text = Math.round(diff / 86400) + ' days ago'
-      } else if (diff < 380 * 86400) {
-        text = Math.round(diff / 30.4 / 86400) + ' months ago'
-      } else {
-        text = Math.round(diff / 365.25 / 86400) + ' years ago'
-      }
-
-      return '<span title="' + htmlentities(date) + '">' + text + '</span>'
     })
   }
 }
--- inc/twig_show.php
+++ inc/twig_show.php
@@ -36,44 +36,4 @@ register_hook("twig_init", function() {
       return $ob->title();
     }, get_db_table($table)->get_entries($filter));
   }));
-
-  $twig->addFilter(new Twig_SimpleFilter('age', function($date) {
-    if ($date == null) {
-      return '';
-    }
-
-    $now = new DateTime();
-    $date1 = new DateTime($date);
-    $diff = $now->getTimestamp() - $date1->getTimestamp();
-
-    if ($diff < 0) {
-      $text = "not yet";
-    }
-    else if ($diff < 2 * 60) {
-      $text = "just now";
-    }
-    elseif ($diff < 45 * 60) {
-      $text = round($diff / 60) . " minutes ago";
-    }
-    elseif ($diff < 90 * 60) {
-      $text = "an hour ago";
-    }
-    elseif ($diff < 24 * 3600) {
-      $text = round($diff / 3600) . " hours ago";
-    }
-    elseif ($diff < 48 * 3600) {
-      $text = "yesterday";
-    }
-    elseif ($diff < 61 * 86400) {
-      $text = round($diff / 86400) . " days ago";
-    }
-    elseif ($diff < 380 * 86400) {
-      $text = round($diff / 30.4 / 86400) . " months ago";
-    }
-    else {
-      $text = round($diff / 365.25 / 86400) . " years ago";
-    }
-
-    return "<span title=\"" . htmlspecialchars($date) . "\">{$text}</span>";
-  }, array("is_safe"=>array("html"))));
 });
